5afd0064f066a59da8d4ebb1cf98134f230db1ac
ilyassoussi <ilyassoussamaanass123@gmail.com>
2025-06-18 05:32:56 +0100

    partie deploy preprod

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project = "ton-projet-id"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "moussaada-cluster"
+  location = "us-central1-a"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  networking_mode = "VPC_NATIVE"
+  network         = "default"
+  subnetwork      = "default"
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "default-pool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.primary.name
+
+  node_config {
+    machine_type = "e2-small"  # <- free tier compatible
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    disk_size_gb = 30
+  }
+
+  initial_node_count = 1
+}
